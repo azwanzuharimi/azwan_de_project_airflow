5afc5586cf7d16cff635f441cb512671b1c85bc6
Azwan_Desktop <ajwan92@gmail.com>
2024-05-30 03:54:02 +0800

    updated Dockerfilemore files

--- working_files/dags/dag_ETL_pricecatcher.py
+++ working_files/dags/dag_ETL_pricecatcher.py
@@ -7,8 +7,9 @@ from pathlib import Path
 default_args = {
     'owner' : 'airflow',
     'start_date' : datetime(2024,4,25),
-    'retries' : 0,
-    'retry_delay' : timedelta(seconds = 30)
+    'retries' : 1,
+    'retry_delay' : timedelta(minutes= 30),
+    "depends_on_past" : False
 }
 
 dag = DAG("ETL_Price_Catcher_DAG", default_args=default_args, schedule=timedelta(days=1))
